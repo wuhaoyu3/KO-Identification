262f7ec1434fb62610ff97b7886b917306dd8f72
Haoyu Wu <108792992+wuhaoyu3@users.noreply.github.com>
2023-11-14 15:05:33 +0800

    Update cluster.py
    bug fixs

--- cluster.py
+++ cluster.py
@@ -6,15 +6,16 @@ from bio_embeddings.extract import pairwise_distance_matrix_from_embeddings_and_
 from sklearn.metrics.pairwise import cosine_similarity
 import copy
 import os
+import sys
 
-result_dir = 'result'
 ref_list = 'ref_list'
 target_list = 'pair_list'
 target_result = 'pair_result'
-result_top5 = 'result_top5'
+result_file = 'result'
 
-ref_file = 'ref.h5'
-target_file = 'target.h5'
+target_file = sys.argv[1]
+ref_file = sys.argv[2]
+result_dir = sys.argv[3]
 
 matrix, list1, list2 = pairwise_distance_matrix_from_embeddings_and_annotations(target_file, ref_file,n_jobs=8)
 
@@ -29,18 +30,14 @@ np.save(os.path.join(result_dir, target_list+'.npy'), a)
 a = np.array(list2)
 np.save(os.path.join(result_dir, ref_list+'.npy'), a)
 
-# top5
-with open(os.path.join(result_dir, result_top5+'.csv'), 'w') as f:
+# get best match
+with open(os.path.join(result_dir, result_file+'.csv'), 'w') as f:
     for i in range(len(matrix)):
         f.write("\"" + list1[i]+"\",")
         m = list(matrix[i])
-        t = copy.deepcopy(m)
-        for _ in range(5):
-            number = min(t)
-            index = t.index(number)
-            t[index] = 1000
-            f.write(str(number)+",")
-            f.write("\""+list2[index].split()[0]+'\",\"'+list2[index].split()[1]+'\",')
+        number = min(m)
+        index = m.index(number)
+        f.write(str(number)+",")
+        f.write("\""+list2[index].split()[0]+'\",\"'+list2[index].split()[1]+'\",')
         print("\r{}".format(i).ljust(20), end='', flush=True)
-        t = []
         f.write("\n")
