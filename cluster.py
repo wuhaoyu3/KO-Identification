ac0504b865b68d53da30ff5a33ff78a7f3e654a3
Haoyu Wu <6213113120@stu.jiangnan.edu.cn>
2023-11-27 16:13:20 +0800

    bug fix

--- cluster.py
+++ cluster.py
@@ -1,37 +1,52 @@
 import numpy as np
 import h5py
+from typing import List, Tuple
 import pandas as pd
-from pandas import read_excel, DataFrame
-from bio_embeddings.extract import pairwise_distance_matrix_from_embeddings_and_annotations
-from sklearn.metrics.pairwise import cosine_similarity
-import copy
-import os
+from sklearn.metrics import pairwise_distances as _pairwise_distances
 import sys
 
-ref_list = 'ref_list'
-target_list = 'pair_list'
-target_result = 'pair_result'
-result_file = 'result'
+query_embeddings_path = sys.argv[1]
+reference_embeddings_path = sys.argv[2]
+result_file_path = sys.argv[3]
 
-target_file = sys.argv[1]
-ref_file = sys.argv[2]
-result_dir = sys.argv[3]
+references: List[str]
+reference_embeddings = list()
 
-matrix, list1, list2 = pairwise_distance_matrix_from_embeddings_and_annotations(target_file, ref_file,n_jobs=8)
+with h5py.File(reference_embeddings_path, 'r') as reference_embeddings_file:
 
-print(len(matrix))
-print(len(matrix[0]))
+    references = list(reference_embeddings_file.keys())
 
-# save
-a = np.array(matrix)
-np.save(os.path.join(result_dir, target_result+'.npy'), a)
-a = np.array(list1)
-np.save(os.path.join(result_dir, target_list+'.npy'), a)
-a = np.array(list2)
-np.save(os.path.join(result_dir, ref_list+'.npy'), a)
+    length = len(references)
+    cur = 0
+    for refereince_identifier in references:
+        reference_embeddings.append(np.array(reference_embeddings_file[refereince_identifier]))
+        cur += 1
+        print(f"\rreference: {cur}/{length}".ljust(20), end='', flush=True)
+
+queries: List[str]
+query_embeddings = list()
+
+with h5py.File(query_embeddings_path, 'r') as query_embeddings_file:
+
+    queries = list(query_embeddings_file.keys())
+
+    length = len(queries)
+    cur = 0
+    for query_identifier in queries:
+        query_embeddings.append(np.array(query_embeddings_file[query_identifier]))
+        cur += 1
+        print(f"\rquery: {cur}/{length}".ljust(20), end='', flush=True)
+
+
+pairwise_distances = _pairwise_distances(
+        query_embeddings,
+        reference_embeddings,
+        metric='euclidean',
+        n_jobs=-1
+)
 
 # get best match
-with open(os.path.join(result_dir, result_file+'.csv'), 'w') as f:
+with open(result_file_path, 'w') as f:
     for i in range(len(matrix)):
         f.write("\"" + list1[i]+"\",")
         m = list(matrix[i])
